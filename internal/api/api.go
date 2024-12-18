92d2a96c267e9b4560d787aa208091da08335531
0xPeterSatoshi <16406061+0xpetersatoshi@users.noreply.github.com>
2024-12-17 21:35:46 -0800

    refactor(api): add app metadata to message and store in in-memory cache

--- internal/api/api.go
+++ internal/api/api.go
@@ -9,17 +9,20 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/patrickmn/go-cache"
 	"github.com/rs/zerolog"
 )
 
 type Message struct {
-	Id       uint32
-	Appid    uint32
-	Message  string
-	Title    string
-	Priority uint32
-	Extras   map[string]interface{}
-	Date     time.Time
+	Id             uint32
+	Appid          uint32
+	AppName        string
+	AppDescription string
+	Message        string
+	Title          string
+	Priority       uint32
+	Extras         map[string]interface{}
+	Date           time.Time
 }
 
 type Application struct {
@@ -40,15 +43,18 @@ type Client struct {
 	clientToken string
 	conn        *websocket.Conn
 	logger      *zerolog.Logger
+	cache       *cache.Cache
 }
 
 // NewClient creates a new gotify API client
 func NewClient(host, clientToken string, ssl bool, logger *zerolog.Logger) *Client {
+	cache := cache.New(60*time.Minute, 120*time.Minute)
 	return &Client{
 		host:        host,
 		clientToken: clientToken,
 		logger:      logger,
 		ssl:         ssl,
+		cache:       cache,
 	}
 }
 
@@ -97,6 +103,23 @@ func (c *Client) ReadMessages(messages chan<- Message) error {
 			continue
 		}
 
+		// add app name and description to message
+		appItem, found := c.cache.Get(fmt.Sprintf("%d", msg.Appid))
+		if found {
+			app := appItem.(Application)
+			msg.AppName = app.Name
+			msg.AppDescription = app.Description
+		} else {
+			app, err := c.getApplicationByID(msg.Appid)
+			if err != nil {
+				c.logger.Error().Err(err).Msg("failed to get application from gotify server")
+				continue
+			}
+			c.cache.SetDefault(fmt.Sprintf("%d", msg.Appid), *app)
+			msg.AppName = app.Name
+			msg.AppDescription = app.Description
+		}
+
 		messages <- msg
 
 		c.logger.Info().Msg("message received from gotify server and sent to channel")
@@ -140,8 +163,8 @@ func (c *Client) makeRequest(method string, endpoint string, body *bytes.Buffer)
 	return res, nil
 }
 
-// GetApplications returns a list of applications
-func (c *Client) GetApplications() ([]Application, error) {
+// getApplications returns a list of applications
+func (c *Client) getApplications() ([]Application, error) {
 	endpoint := c.host + "/application?token=" + c.clientToken
 
 	res, err := c.makeRequest("GET", endpoint, nil)
@@ -157,4 +180,20 @@ func (c *Client) GetApplications() ([]Application, error) {
 	}
 
 	return applications, nil
+}
+
+// getApplicationByID returns an application by id
+func (c *Client) getApplicationByID(id uint32) (*Application, error) {
+	applications, err := c.getApplications()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, application := range applications {
+		if application.ID == id {
+			return &application, nil
+		}
+	}
+
+	return nil, fmt.Errorf("application with id %d not found", id)
 }
